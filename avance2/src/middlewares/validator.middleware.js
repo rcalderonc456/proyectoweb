2e52ab006b0d2915dc80e59bbd1e4966c1780cf4
rcalderonc456 <123522261+rcalderonc456@users.noreply.github.com>
2024-08-02 18:59:54 -0600

    Delete avance2 directory

--- avance2/src/middlewares/validator.middleware.js
+++
@@ -1,9 +0,0 @@
-export const validateSchema = (schema) => (req, res, next) =>{
-    try {
-        schema.parse(req.body);
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({message: error.errors.map((error) => error.message)});
-    }
-}
--- avance2/src/schemas/proveedores.schema.js
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const createProveedorSchema = z.object({
-    id: z.string({ required_error: "El ID del proveedor es obligatorio" }),
-    nombre_proveedor: z.string({ required_error: "El nombre del proveedor es obligatorio" }),
-    contacto: z.number({ required_error: "El contacto es obligatorio" }),
-    direccion: z.string({ required_error: "La dirección es obligatoria" })
-});
